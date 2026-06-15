813fe413354ccd14f930c94cd51c0fae5bbf0562
anilpreetham <anilpreetham2322@gmail.com>
2026-06-15 13:57:17 +0530

    update for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const MenuItem = require("./models/MenuItem");
 const Order = require("./models/Order");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
 app.use(express.json());
 
 mongoose
